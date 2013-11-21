c3039a3b95dc9024a442624284e7683bc0153644
flar2 <asegaert@gmail.com>
2013-11-20 21:36:09 -0500

    Option to disable touch wake with power button
    Signed-off-by: flar2 <asegaert@gmail.com>

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -27,6 +27,10 @@
 
 #include <asm/system_info.h>
 
+#ifdef CONFIG_PWRKEY_SUSPEND
+#include <linux/qpnp/power-on.h>
+#endif
+
 #define DT_CMD_HDR 6
 #define GAMMA_COMPAT 11
 
@@ -200,6 +204,11 @@ void mdss_dsi_panel_reset(struct mdss_panel_data *pdata, int enable)
 	prevent_sleep = prevent_sleep || (dt2w_switch > 0);
 #endif
 #endif
+#ifdef CONFIG_PWRKEY_SUSPEND
+	if (pwrkey_pressed)
+		prevent_sleep = false;
+#endif
+
 	if (pdata == NULL) {
 		pr_err("%s: Invalid input data\n", __func__);
 		return;
@@ -333,6 +342,10 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 	if (local_pdata->on_cmds.cmd_cnt)
 		mdss_dsi_panel_cmds_send(ctrl, &local_pdata->on_cmds);
 
+#ifdef CONFIG_PWRKEY_SUSPEND
+	pwrkey_pressed = false;	
+#endif
+		
 	pr_info("%s\n", __func__);
 	return 0;
 }
@@ -352,6 +365,11 @@ static int mdss_dsi_panel_off(struct mdss_panel_data *pdata)
 	prevent_sleep = prevent_sleep || (dt2w_switch > 0);
 #endif
 #endif
+#ifdef CONFIG_PWRKEY_SUSPEND
+	if (pwrkey_pressed)
+		prevent_sleep = false;
+#endif
+
 	if (pdata == NULL) {
 		pr_err("%s: Invalid input data\n", __func__);
 		return -EINVAL;
